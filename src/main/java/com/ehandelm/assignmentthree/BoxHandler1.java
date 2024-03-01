e0ffa3a2ac1aeeb85b24a7d47999fb8bcb412c3e
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2024-02-29 21:03:55 -0800

    Update BoxHandler1.java

--- src/main/java/com/ehandelm/assignmentthree/BoxHandler1.java
+++ src/main/java/com/ehandelm/assignmentthree/BoxHandler1.java
@@ -3,11 +3,20 @@ package com.example.asgn3;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 
+/**
+ *
+ * Handler class for first column of boxes (i.e. boxes for 0<x<30 and 60<x<90)
+ * Miro Haapalainen, Saylor Benes
+ */
+
 public class BoxHandler1 extends BoxHandler {
 
-    Rectangle rect1;
-    Rectangle rect2;
+    private Rectangle rect1;
+    private Rectangle rect2;
 
+    /**
+     * On class instantiation, instantiate rectangle backends.
+     */
     public BoxHandler1() {
         this.rect1 = new Rectangle();
         this.rect1.setWidth(5);
@@ -17,11 +26,12 @@ public class BoxHandler1 extends BoxHandler {
         this.rect2.setHeight(5);
     }
 
+    @Override
     public void expandBox1() {
         this.rect1.setWidth(this.rect1.getWidth()*1.5);
         this.rect1.setWidth(this.rect1.getHeight()*1.5);
     }
-
+    @Override
     public void expandBox2() {
         this.rect2.setWidth(this.rect2.getWidth()*1.5);
         this.rect2.setWidth(this.rect2.getHeight()*1.5);
@@ -36,5 +46,11 @@ public class BoxHandler1 extends BoxHandler {
         }
     }
 
+    public Rectangle getRect1() {
+        return rect1;
+    }
 
+    public Rectangle getRect2() {
+        return rect2;
+    }
 }
