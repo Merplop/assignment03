123e15079171a1bccb00f1d24c490bda1f68e06f
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2024-02-29 21:03:43 -0800

    Update BoxHandler.java

--- src/main/java/com/ehandelm/assignmentthree/BoxHandler.java
+++ src/main/java/com/ehandelm/assignmentthree/BoxHandler.java
@@ -7,6 +7,12 @@ import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+/**
+ *
+ * Abstract class which provides pattern for Box Handler subclasses
+ * Miro Haapalainen, Saylor Benes
+ */
+
 public abstract class BoxHandler {
     private AnchorPane graphVisual;
     private BoxHandler next;
@@ -14,5 +20,35 @@ public abstract class BoxHandler {
     public BoxHandler() {
     }
 
+    /**
+     * Method which handles the size of the randomly generated value
+     * @param size
+     */
+
     public abstract void handle(int size);
+
+    /**
+     * Method which expands first box
+     */
+
+    public abstract void expandBox1();
+
+    /**
+     * Method which expands second box
+     */
+
+    public abstract void expandBox2();
+
+    /**
+     * Set next handler in chain
+     * @param b next BoxHandler in chain of responsibility
+     */
+
+    public void setNext(BoxHandler b) {
+        this.next = b;
+    }
+
+    public BoxHandler getNext() {
+        return next;
+    }
 }
